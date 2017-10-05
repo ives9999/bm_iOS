c2131e4224ea6647e2db40f956af752ff3d6848a
ives sun <ives@housetube.tw>
2017-10-05 15:24:32 +0800

    adjust template.

--- bm/Controllers/HomeVC.swift
+++ bm/Controllers/HomeVC.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 
-class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
 
     @IBOutlet weak var homeCV: UICollectionView!
     
     private(set) public var homes = [Home]()
+    private(set) public var frameWidth: CGFloat?
+    //var deviceType: DeviceType?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        frameWidth = view.frame.width
+        
+//        switch UIDevice.current.userInterfaceIdiom {
+//        case .phone:
+//            type = "phone"
+//        case .pad:
+//            type = "pad"
+//        case .unspecified:
+//            type = "phone"
+//        }
+        
         homeCV.delegate = self
         homeCV.dataSource = self
         
         homes = DataService.instance.getHomes()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //print(frameWidth)
+        let cellCount = (CGFloat)(UIDevice.current.userInterfaceIdiom == .phone ? 1 : 2)
+        let cellWidth = frameWidth! / cellCount - 10
+        let cellHeight = (CGFloat)(UIDevice.current.userInterfaceIdiom == .phone ? 245 : 425)
+        let size = CGSize(width: cellWidth, height: cellHeight)
+        return size
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
