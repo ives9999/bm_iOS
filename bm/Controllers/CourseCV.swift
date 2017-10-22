dd4712f502fd9c373fc58b6457436a5a9539261a
ives <ives@housetube.tw>
2017-10-22 20:00:03 +0800

    course start.

---
+++ bm/Controllers/CourseCV.swift
@@ -0,0 +1,18 @@
+//
+//  CourseCV.swift
+//  bm
+//
+//  Created by ives on 2017/10/22.
+//  Copyright © 2017年 bm. All rights reserved.
+//
+
+import UIKit
+
+class CourseCV: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
