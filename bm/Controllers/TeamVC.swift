69d9299f38fcfe0e7d6c209225210a72e7c1063d
ives <ives@housetube.tw>
2018-05-21 12:14:39 +0800

    1.0.4 start.

--- bm/Controllers/TeamVC.swift
+++ bm/Controllers/TeamVC.swift
@@ -13,13 +13,22 @@ class TeamVC: ListVC {
     
     let _type:String = "team"
     @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var managerBtn: UIButton!
     
     override func viewDidLoad() {
         setIden(item:_type, titleField: "name")
         super.viewDidLoad()
         Global.instance.setupTabbar(self)
+        managerBtn.layer.cornerRadius = 12
         refresh()
         Global.instance.menuPressedAction(menuBtn, self)
     }
     
+    @IBAction func manager(_ sender: Any) {
+        if !Member.instance.isLoggedIn {
+            SCLAlertView().showError("警告", subTitle: "請先登入為會員")
+        } else {
+            performSegue(withIdentifier: "segue", sender: nil)
+        }
+    }
 }
