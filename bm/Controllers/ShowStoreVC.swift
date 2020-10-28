3612828dbbc62f9cd8482ff9ecdd0a551aaf781c
ives <ives@housetube.tw>
2020-10-28 12:41:17 +0800

    store content start.

--- bm/Controllers/ShowStoreVC.swift
+++ bm/Controllers/ShowStoreVC.swift
@@ -13,6 +13,8 @@ class ShowStoreVC: BaseViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var scrollContainerView: UIView!
     
+    @IBOutlet weak var featured: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,6 +34,39 @@ class ShowStoreVC: BaseViewController {
         
         beginRefresh()
         scrollView.addSubview(refreshControl)
-        refresh()
+        //refresh()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.x != 0 {
+            scrollView.contentOffset.x = 0
+        }
+    }
+    
+    func changeScrollViewContentSize() {
+        
+        let h1 = featured.bounds.size.height
+//        let h2 = courseDataLbl.bounds.size.height
+//        let h3 = tableViewConstraintHeight.constant
+//        let h4 = coachDataLbl.bounds.size.height
+//        let h5 = coachTableViewConstraintHeight.constant
+//        let h6 = contentLbl.bounds.size.height
+//        let h7 = contentViewConstraintHeight!.constant
+//        let h8 = signupDataLbl.bounds.size.height
+//        let h9 = signupTableViewConstraintHeight.constant
+        //print(contentViewConstraintHeight)
+        
+        //let h: CGFloat = h1 + h2 + h3 + h4 + h5
+//        let h: CGFloat = h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9 + 300
+//        scrollView.contentSize = CGSize(width: view.frame.width, height: h)
+//        ContainerViewConstraintHeight.constant = h
+        //print(h1)
+    }
+    
+    @IBAction func prevBtnPressed(_ sender: Any) {
+//        if delegate != nil {
+//            delegate!.isReload(false)
+//        }
+        prev()
     }
 }
