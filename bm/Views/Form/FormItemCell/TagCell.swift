c906ea52a36b32f96a48ff28f0194656ad9f52bc
ives <ives@housetube.tw>
2021-01-11 12:51:27 +0800

    tag cell start.

---
+++ bm/Views/Form/FormItemCell/TagCell.swift
@@ -0,0 +1,20 @@
+//
+//  TagCell.swift
+//  bm
+//
+//  Created by ives on 2021/1/11.
+//  Copyright © 2021 bm. All rights reserved.
+//
+
+import UIKit
+
+class TagCell: FormItemCell {
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var containViewHeight: NSLayoutConstraint!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
