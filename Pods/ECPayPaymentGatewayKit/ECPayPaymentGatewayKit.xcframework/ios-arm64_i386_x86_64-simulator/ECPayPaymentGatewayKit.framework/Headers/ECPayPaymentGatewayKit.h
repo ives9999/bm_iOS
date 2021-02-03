c228f35c2c9d5f204b055d2037fe1c0692a50063
ives <ives@housetube.tw>
2021-02-03 10:40:26 +0800

    add ecpay pods.

---
+++ Pods/ECPayPaymentGatewayKit/ECPayPaymentGatewayKit.xcframework/ios-arm64_i386_x86_64-simulator/ECPayPaymentGatewayKit.framework/Headers/ECPayPaymentGatewayKit.h
@@ -0,0 +1,21 @@
+//
+//  ECPayPaymentGatewayKit.h
+//  ECPayPaymentGatewayKit
+//
+//  Created by Mingfeng Ho 何明峯 on 2020/5/5.
+//  Copyright © 2020 ECPay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ECPayPaymentGatewayKit.
+FOUNDATION_EXPORT double ECPayPaymentGatewayKitVersionNumber;
+
+//! Project version string for ECPayPaymentGatewayKit.
+FOUNDATION_EXPORT const unsigned char ECPayPaymentGatewayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECPayPaymentGatewayKit/PublicHeader.h>
+
+
+//#import "NSData+SHA.h"
+#import "SwiftyRSA.h"
