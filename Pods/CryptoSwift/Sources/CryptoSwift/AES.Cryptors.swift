1a5cf525c77de732262549f049c9838618a9a4a6
ives <ives@housetube.tw>
2021-02-05 15:11:22 +0800

    no message

--- Pods/CryptoSwift/Sources/CryptoSwift/AES.Cryptors.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/AES.Cryptors.swift
@@ -16,6 +16,7 @@
 // MARK: Cryptors
 
 extension AES: Cryptors {
+    
     public func makeEncryptor() throws -> Cryptor & Updatable {
         let worker = try blockMode.worker(blockSize: AES.blockSize, cipherOperation: encrypt)
         if worker is StreamModeWorker {
